0622696ae9418dc557615fb94246b84478e7d45a
KŽ <kristopherzuravljov005@gmail.com>
2026-05-20 11:49:07 +0300

    Layout total change, the main game is now in main page wich acts as gamehub, where you can go straight to game, go to shop, go to settings wich used to be in merge is tooken out to main page, the game finally looks like official game, the old stuff was send to dropdown selection list called dev stuff it only seen when admin is on

--- KalapulgaMerge/Controllers/HomeController.cs
+++ KalapulgaMerge/Controllers/HomeController.cs
@@ -1,20 +1,48 @@
 using System.Diagnostics;
+using KalapulgaMerge.Data;
 using KalapulgaMerge.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace KalapulgaMerge.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly KalapulkDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, KalapulkDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            ViewBag.Coins = 0;
+            ViewBag.IsLoggedIn = false;
+            ViewBag.PlayerName = "Guest";
+            ViewBag.ProfilePic = null;
+
+            var userIdText = HttpContext.Session.GetString("UserId");
+
+            if (int.TryParse(userIdText, out var userId))
+            {
+                ViewBag.IsLoggedIn = true;
+                ViewBag.PlayerName = HttpContext.Session.GetString("UserName") ?? "Player";
+                ViewBag.ProfilePic = HttpContext.Session.GetString("UserProfilePic");
+
+                try
+                {
+                    var state = await _context.MergePlayerStates.FirstOrDefaultAsync(x => x.UserAccountId == userId);
+                    ViewBag.Coins = state?.Coins ?? 0;
+                }
+                catch
+                {
+                    ViewBag.Coins = 0;
+                }
+            }
+
             return View();
         }
 
